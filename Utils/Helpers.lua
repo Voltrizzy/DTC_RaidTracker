e3c8fa25c43b0626956e385c42c6ab1f7289a80b
Voltrizzy <mike.kamstra@gmail.com>
2026-01-29 21:22:00 -0600

    Update Helpers.lua

--- Utils/Helpers.lua
+++ Utils/Helpers.lua
@@ -17,4 +17,28 @@ function DTC.Utils:GetAnnounceName(fullName)
     end
     
     return fullName
+
+-- Add to your existing table, e.g., DTC.Utils or local Utils
+function DTC.Utils.CanOpenVoteWindow()
+    local _, instanceType = GetInstanceInfo()
+
+    -- instanceType return values: "none" (Open World), "party" (Dungeon), "raid", "pvp", "arena"
+
+    -- 1. STRICTLY BLOCK 5-man Dungeons
+    if instanceType == "party" then
+        return false
+    end
+
+    -- 2. ALLOW Raids
+    if instanceType == "raid" then
+        return true
+    end
+
+    -- 3. ALLOW Open World (instanceType is "none") for testing
+    if instanceType == "none" then
+        return true
+    end
+
+    -- Default: Block everything else (Scenarios, PVP, Arena) to be safe
+    return false
 end
